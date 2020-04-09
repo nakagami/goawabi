8869cb3459e199519cc9ecf57760666ccb70303d
Hajime Nakagami <nakagami@gmail.com>
2020-04-09 21:08:48 +0900

    implement goawabi command

--- cmd/goawabi/main.go
+++ cmd/goawabi/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"os"
+	"io/ioutil"
+	"github.com/nakagami/goawabi"
+)
 
 func main() {
-	fmt.Printf("Hello World\n")
+	tokenizer, err := goawabi.NewTokenizer("")
+	if err != nil {
+		panic(err)
+	}
+
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	morphemes, err := tokenizer.Tokenize(strings.TrimSpace(string(input)))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, m := range morphemes {
+		fmt.Printf("%s\t%s\n", m[0], m[1])
+	}
+	fmt.Printf("EOS\n")
 }
