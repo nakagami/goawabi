6d567eedb730dbc26d6a97084bbd4ffe2b93a7a9
Hajime Nakagami <nakagami@gmail.com>
2020-04-11 15:36:44 +0900

    -N parameter

--- cmd/goawabi/main.go
+++ cmd/goawabi/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"strings"
-	"os"
-	"io/ioutil"
 	"github.com/nakagami/goawabi"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
+func print(morphemes [][2]string) {
+	for _, m := range morphemes {
+		fmt.Printf("%s\t%s\n", m[0], m[1])
+	}
+	fmt.Printf("EOS\n")
+}
+
 func main() {
+	var (
+		n = flag.Int("N", 1, "N best")
+	)
+
 	tokenizer, err := goawabi.NewTokenizer("")
 	if err != nil {
 		panic(err)
@@ -19,13 +31,20 @@ func main() {
 		panic(err)
 	}
 
-	morphemes, err := tokenizer.Tokenize(strings.TrimSpace(string(input)))
-	if err != nil {
-		panic(err)
-	}
+	if *n > 1 {
+		morphemes_list, err := tokenizer.TokenizeNBest(strings.TrimSpace(string(input)), *n)
+		if err != nil {
+			panic(err)
+		}
+		for _, m := range morphemes_list {
+			print(m)
+		}
 
-	for _, m := range morphemes {
-		fmt.Printf("%s\t%s\n", m[0], m[1])
+	} else {
+		morphemes, err := tokenizer.Tokenize(strings.TrimSpace(string(input)))
+		if err != nil {
+			panic(err)
+		}
+		print(morphemes)
 	}
-	fmt.Printf("EOS\n")
 }
